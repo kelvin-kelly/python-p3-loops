3961f233aa2675095df0ae1889d4396209ad4d4e
Kelvin Machira <machirakelvin81@gmail.com>
2024-05-25 14:39:19 +0300

    FizzBuzz

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,32 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+ n = 10 
+ while n > 0:
+    print(n)
+    n -= 1
+    print("Happy New Year!")
+pass
 
 def square_integers(int_list):
     # code goes here!
+    return [num**2 for num in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+
+ for i in range (100):
+     i <= 100
+     i += 1
+     if i % 3 == 0 and i % 5 == 0 :
+      print("FizzBuzz")
+    
+     elif i % 3 == 0 :
+      print("Fizz")
+    
+     elif i % 5 == 0 :
+      print("Buzz")
+     
+     else : 
+        print(i)
+pass
